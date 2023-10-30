32d4a8948e4eea3c3a45ed7d8bf19ec87bbbc765
Henrietta Obioha <henriettaobioha1@gmail.com>
2023-10-30 17:52:57 +0000

    updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,6 +6,7 @@ Create a Flask app; and register the blueprint app_views to Flask instance app.
 from os import getenv
 from flask import Flask, jsonify
 from flask_cors import CORS
+
 from models import storage
 from api.v1.views import app_views
 
